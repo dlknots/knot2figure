903c8bb5aa7bc8e08478d4aae9081a515c44b673
dpknots-bnu <105548591+dpknots-bnu@users.noreply.github.com>
2022-05-14 20:57:43 +0800

    Update example.py

--- example.py
+++ example.py
@@ -6,25 +6,16 @@ Created on Thu Nov 18 22:47:34 2021
 """
 
 from figure2knot2figure import f2k
-# from f2kk2f import f2k
 
-import time
 '''
 需要将图片放在同级文件夹或pth赋完整路径
 '''
-start = time.time()
+filename="figures/"
+#需要转化的图片名
+pth=filename+"knot01.png"
 
-# pth="s14.png"
-pth="knot01.png"
-# pth="knot03.jpg"
-# pth="knotX.jpg"
-# pth="knot_H04.jpg"
-# pth="s5.png"
 a=f2k(pth)
-# k=a.fun1()#v1.0
-# print(len(k[0]))
-k=a.fun2()#v2.0 #返回值为参数曲线坐标
-print(k.shape)
-end = time.time()
-#
-print(end-start)
+a.fun1()#v1.0 #返回值为参数曲线坐标
+a.fun2()#v2.0 #返回值为参数曲线坐标
+
+
